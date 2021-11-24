5a44ad39f2bff8e9f21bf20c12edd9f624374639
Gabababab <gravottagabriele@gmail.com>
2021-11-24 11:36:33 +0100

    effettuate modifiche, aggiunti status code in controller

--- src/main/java/it/prova/gestionedottori/GestionedottoriApplication.java
+++ src/main/java/it/prova/gestionedottori/GestionedottoriApplication.java
@@ -1,28 +1,29 @@
 package it.prova.gestionedottori;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import it.prova.gestionedottori.model.Dottore;
-import it.prova.gestionedottori.repository.DottoreRepository;
+import it.prova.gestionedottori.service.DottoreService;
 
 @SpringBootApplication
-public class GestionedottoriApplication implements CommandLineRunner  {
+public class GestionedottoriApplication {
 
-	@Autowired
-	private DottoreRepository repository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(GestionedottoriApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		repository.save(new Dottore("paolo", "rossi", "CJSH89", true, false));
-		repository.save(new Dottore("paolo2", "rossi2", "2CJSH89", true, false));
-		repository.save(new Dottore("Carlo", "rossi", "FDDG67", false, false));
-	}
 
+	@Bean
+	public CommandLineRunner initDottori(DottoreService service) {
+		return (args) -> {
+			
+			service.save(new Dottore("paolo", "rossi", "CJSH89", true, false));
+			service.save(new Dottore("paolo2", "rossi2", "2CJSH89", true, false));
+			service.save(new Dottore("Carlo", "rossi", "FDDG67", false, false));
+		};
+	}
 }
